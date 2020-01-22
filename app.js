233904f441394d7ab43f3017069d9bb61fd8ea1c
Nate Hodge <nhodge253@gmail.com>
2020-01-21 19:55:11 -0800

    added image uploading functionality

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
-
+const uploadRouter = require("./routes/uploadRouter");
 const mongoose = require("mongoose");
 
 const Dishes = require("./models/dishes");
@@ -59,6 +59,7 @@ app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
