0897087405c115ee0f84168c4fd77812125ae815
Nate Hodge <nhodge253@gmail.com>
2020-01-26 16:17:26 -0800

    changed config to hide fb key

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+const apiConfig = "require(./configapi)";
+
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
   facebook: {
-    clientId: "2567839006797272",
-    clientSecret: "2c53e1596e535676f398af45e0b6bbbf"
+    clientId: "apiConfig.ID",
+    clientSecret: "apiConfig.SECRET_KEY"
   }
 };
